aaf269b0f559149a7180e7acee88342458ef4b52
YagoIglessias99 <yagoiglessias66@gmail.com>
2025-05-30 21:49:39 -0300

    feat: lista de usuários

--- src/main/java/views/user/UsuarioList.java
+++ src/main/java/views/user/UsuarioList.java
@@ -7,7 +7,7 @@ import java.awt.*;
 
 import java.util.List;
 public class UsuarioList extends JPanel {
-    private UserListTableModel.UserListTableModel tableModel;
+    private UserListTableModel tableModel;
 
     public UsuarioList(){
         this.initComponents();
@@ -15,7 +15,7 @@ public class UsuarioList extends JPanel {
 
     private void initComponents() {
         setLayout(new BorderLayout());
-        
+
         this.tableModel = new UserListTableModel(List.of());
     }
 }
